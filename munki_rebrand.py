4fd8caf92bb351d1a6ecd43870e5b18218bbf84c
M@ <childrss@users.noreply.github.com>
2016-10-22 06:59:23 -0500

    first re-write

--- munki_rebrand.py
+++ munki_rebrand.py
@@ -7,6 +7,13 @@
 # Customised for University of Oxford by
 # Ben Goodstein Mar 2015
 #
+# Matt "M@" Childress, University of Illinois:
+#   * if it exists, only updates local git repo of munki source
+#     doesn't nuke and redownload each time
+#   * builds multiple installers based on how many postinstall_scripts-* are in root directory
+#   * renames installers based on postinstall_script-(*) name
+#   * commented out a lot of stuff I didn't need
+#
 # Prerequisites: You need Xcode (5/6) installed
 # For Xcode 6 you need to add the 10.8 SDK
 # See: https://github.com/munki/munki/wiki/Building%20Munki2%20Pkgs
@@ -23,24 +30,27 @@
 # ! Caveat: this is a very rudimentary script that is likely
 # to break in future MSC releases. Use at your own risk
 
+import glob
 import fileinput
 import subprocess
 import re
-from os import listdir, stat, chmod
-from os.path import isfile, join
+from os import listdir, stat, chmod, makedirs, rename
+from os.path import isfile, isdir, join
 from shutil import copyfile
 
 # Desired new app name
-appNameWanted = 'Orchard Software Centre'
+appNameWanted = 'Managed Software Center'
 
 # Optional icon file to replace the MSC icon
-srcIcon = 'AppIcon.icns'
+# srcIcon = 'AppIcon.icns'
 
-# Optional postinstall script to be executed upon install
-postinstall_script = 'postinstall_script'
+# Read in a list of files starting with postinstall_script to be executed upon install
+
+postinstall_script_files = glob.glob("./postinstall_script*")
+print ( postinstall_script_files )
 
 # Git release tag (leave empty for latest build)
-tag = 'v2.2.4'
+tag = ''
 
 ### Probably don't need to edit below this line
 
@@ -65,21 +75,49 @@ appNameLocalized = {    'da'       : 'Managed Software Center',
                                 'sv'       : 'Managed Software Center'
                             }
 
-# Git repo
-git_repo = "https://github.com/munki/munki"
 
 # Make Munki pkg script
 make_munki = 'munki/code/tools/make_munki_mpkg.sh'
 
-# First cleanup previous runs
-print 'Cleaning up previous runs'
-proc = subprocess.Popen(['sudo','/bin/rm','-rf', 'munki'])
-proc.communicate()
+# Git repo
+git_repo = "https://github.com/munki/munki"
 
-# Checkout git repo
-print 'Cloning git repo'
-proc = subprocess.Popen(['git','clone', git_repo])
-proc.communicate()
+# if we already have a munki subdirectory, we've already downloaded the repo (no need to 
+if isdir( "./munki" ): 
+
+    '''
+    # code for tessting
+    subprocess.call(["ls", "-l"], cwd='./munki')
+    if isdir ( "./munki/bad-directory" ): 
+        print ( "bad-directory exists" )     
+    else:
+        makedirs ( "./munki/bad-directory" )
+        print ( "creating bad-directory" )     
+        
+    if isfile ( "./munki/bad-file" ):
+        print ( "bad-file exists" )     
+    else:
+        subprocess.call(['touch', join("./munki/", "bad-file")])
+        print ( "creating bad-file" )     
+
+    subprocess.call(["ls", "-l"], cwd='./munki')
+    print ""
+    ''' 
+           
+    # -d clears directories AND fies
+    # -x clears untracked files
+    # shell=True is not advised, but the only way I could get git to work 
+    print 'Cleaning munki git repo'
+    subprocess.call(['/usr/bin/git clean -f -x -d', git_repo], shell=True, cwd='./munki')
+
+    print 'Updating git repo'
+    proc = subprocess.call(['/usr/bin/git fetch', git_repo], shell=True, cwd="./munki")
+    proc = subprocess.call(['/usr/bin/git reset --hard', git_repo], shell=True, cwd="./munki")
+else:
+    # ./munki does exist, so create it 
+    print 'Cloning git repo'
+    proc = subprocess.Popen(['git','clone', git_repo])
+    proc.communicate()
 
 if tag:
       print 'Checkout tag %s' % tag
@@ -121,13 +159,18 @@ for appDir in appDirs:
                                 searchReplace(localizedName, appNameWanted, fileToSearch)
 
 # Copy icons
-if isfile(srcIcon):
-      print("Replace icons with %s" % srcIcon)
-      destIcon = "munki/code/apps/Managed Software Center/Managed Software Center/Managed Software Center.icns"
-      copyfile(srcIcon, destIcon)
-      destIcon = "munki/code/apps/MunkiStatus/MunkiStatus/MunkiStatus.icns"
-      copyfile(srcIcon, destIcon)
+#if isfile(srcIcon):
+#      print("Replace icons with %s" % srcIcon)
+#      destIcon = "munki/code/apps/Managed Software Center/Managed Software Center/Managed Software Center.icns"
+#      copyfile(srcIcon, destIcon)
+#      destIcon = "munki/code/apps/MunkiStatus/MunkiStatus/MunkiStatus.icns"
+#      copyfile(srcIcon, destIcon)
 
+for postinstall_script in postinstall_script_files:
+      
+      # the 18: strips off the first 21 characters, which are "./postinstall_script-" 
+      config_file_directory_name = postinstall_script[ 21: ]
+      print ( config_file_directory_name )
       print("Add postinstall script: %s" % postinstall_script)
       postinstall_dest = "munki/code/pkgtemplate/Scripts_app/postinstall"
       copyfile(postinstall_script, postinstall_dest)
@@ -136,6 +179,13 @@ if isfile(srcIcon):
       chmod(postinstall_dest, (st.st_mode | 0111))
 
 
-print("Building Munki")
-proc = subprocess.Popen(['./munki/code/tools/make_munki_mpkg.sh','-r','munki'])
-proc.communicate()
+      print("Building Munki ")
+      proc = subprocess.Popen(['./munki/code/tools/make_munki_mpkg.sh','-r','munki'])
+      proc.communicate()
+
+      # rename the resultant built .pkg installer file to reflect the imbedded config filename
+      munki_installer_files = glob.glob("munkitools-*.pkg")
+      munki_installer_name = munki_installer_files[0]
+      print ( munki_installer_name )
+      print ( config_file_directory_name + munki_installer_name )
+      rename( munki_installer_name, config_file_directory_name + "-" +munki_installer_name )
